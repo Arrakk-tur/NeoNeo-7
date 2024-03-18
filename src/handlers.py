ba60994e34a455e839e134835295798d07539ca2
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2024-03-18 02:32:53 +0200

    Added stylization for handlers

--- src/handlers.py
+++ src/handlers.py
@@ -2,6 +2,7 @@ from src.error_handler import input_error
 from src.classes import Record, AddressBook
 import re
 
+
 blue = "\033[94m"
 reset = "\033[0m"
 green = "\033[92m"
@@ -15,19 +16,21 @@ def add_contact(args, address_book, is_consent=False):
         name, phone = args
 
     except:
-        raise ValueError("The command is bad. Give me name and phone please.")
+        raise ValueError(
+            f"{red}The command is bad. Give me name and phone please.{reset}\n"
+        )
 
-    # record = address_book.find(name)
-    #
-    # if record and not is_consent:
-    #     return f"Here is a contact with name {name} yet. To overwrite? Type yes/no: "
-    #
-    # if not record:
+        # record = address_book.find(name)
+
+        # if record and not is_consent:
+        #     return f"Here is a contact with name {name} yet. To overwrite? Type yes/no: "
+
+        # if not record:
     record = Record(name)
 
     record.add_phone(phone)
     address_book.add_record(record)
-    return "Contact added."
+    return f"{green}Contact added.{reset}"
 
 
 @input_error
@@ -36,7 +39,9 @@ def change_phone(args, address_book):
     try:
         name, phone = args
     except:
-        raise ValueError("The command is bad. Give me name and phone please.")
+        raise ValueError(
+            f"{red}The command is bad. Give me name and phone please.{reset}\n"
+        )
 
     # if not bool(re.fullmatch(PHONE_MASK, phone)):
     #     raise TypeError
@@ -46,9 +51,9 @@ def change_phone(args, address_book):
     if record:
         record.edit_phone(phone)
         address_book.add_record(record)
-        return "Contact changed"
+        return f"{green}Contact changed.{reset}"
     else:
-        return f"There isn't a contact with name {name}"
+        return f"{red}There isn't a contact with name {name}.{reset}\n"
 
 
 @input_error
@@ -57,16 +62,16 @@ def show_phone(args, address_book):
     try:
         name = args[0]
     except:
-        raise ValueError("The command is bad. Give me name")
+        raise ValueError(f"{red}The command is bad. Give me name.{reset}\n")
 
     record = address_book.find(name)
     if record:
         if record.phone:
-            return f"Phone of {name}: {record.phone}"
+            return f"{green}Phone of {name}: {record.phone}.{reset}"
         else:
-            return f"Phone not set for {name}"
+            return f"{red}Phone not set for {name}.{reset}\n"
     else:
-        return f"No contact found with name {name}"
+        return f"{red}No contact found with name {name}.{reset}\n"
 
 
 @input_error
@@ -88,16 +93,16 @@ def add_birthday(args, address_book):
         name, birthday = args
     except:
         raise ValueError(
-            "The command is bad. Give me name and birthday in format DD.MM.YYYY."
+            f"{red}The command is bad. Give me name and birthday in format DD.MM.YYYY.{reset}\n "
         )
 
     record = address_book.find(name)
     if record:
         record.add_birthday(birthday)
         address_book.add_record(record)
-        return f"Birthday added for {name}"
+        return f"{green}Birthday added for {name}.{reset}"
     else:
-        return f"No contact found with name {name}"
+        return f"{red}No contact found with name {name}.{reset}\n"
 
 
 @input_error
@@ -105,16 +110,16 @@ def show_birthday(args, address_book):
     try:
         name = args[0]
     except:
-        raise ValueError("The command is bad. Give me name")
+        raise ValueError(f"{red}The command is bad. Give me name.{reset}\n")
 
     record = address_book.find(name)
     if record:
         if record.birthday:
-            return f"Birthday of {name}: {record.birthday}"
+            return f"{green}Birthday of {name}: {record.birthday}.{reset}"
         else:
-            return f"Birthday not set for {name}"
+            return f"{red}Birthday not set for {name}.{reset}\n"
     else:
-        return f"No contact found with name {name}"
+        return f"{red}No contact found with name {name}.{reset}\n"
 
 
 @input_error
@@ -136,15 +141,15 @@ def add_address(args, address_book):
     try:
         name, address = args
     except:
-        raise ValueError("The command is bad. Give me name and address.")
+        raise ValueError(f"{red}The command is bad. Give me name and address.{reset}\n")
 
     record = address_book.find(name)
     if record:
         record.add_address(address)
         address_book.add_record(record)
-        return f"Address added for {name}"
+        return f"{green}Address added for {name}.{reset}"
     else:
-        return f"No contact found with name {name}"
+        return f"{red}No contact found with name {name}.{reset}\n"
 
 
 @input_error
@@ -154,7 +159,7 @@ def change_address(args, address_book):
         name, old_address, new_address = args
     except:
         raise ValueError(
-            "The command is bad. Give me name, old address and new address."
+            f"{red}The command is bad. Give me name, old address and new address.{reset}\n"
         )
 
     record = address_book.find(name)
@@ -162,9 +167,9 @@ def change_address(args, address_book):
     if record:
         record.edit_address(old_address, new_address)
         address_book.add_record(record)
-        return "Address changed"
+        return f"{green}Address changed.{reset}"
     else:
-        return f"There isn't a contact with name {name}"
+        return f"{red}There isn't a contact with name {name}.{reset}\n"
 
 
 @input_error
@@ -172,16 +177,16 @@ def show_address(args, address_book):
     try:
         name = args[0]
     except:
-        raise ValueError("The command is bad. Give me name")
+        raise ValueError(f"{red}The command is bad. Give me name.{reset}\n")
 
     record = address_book.find(name)
     if record:
         if record.address:
-            return f"Address of {name}: {record.address}"
+            return f"{green}Address of {name}: {record.address}.{reset}"
         else:
-            return f"Address not set for {name}"
+            return f"{red}Address not set for {name}.{reset}\n"
     else:
-        return f"No contact found with name {name}"
+        return f"{red}No contact found with name {name}.{reset}\n"
 
 
 @input_error
@@ -192,16 +197,16 @@ def delete_address(args, address_book):
         name = args[0]
     except:
 
-        raise ValueError("The command is bad. Give me name.")
+        raise ValueError(f"{red}The command is bad. Give me name.{reset}\n")
     record = address_book.find(name)
     if record:
 
         record.remove_address()
         address_book.add_record(record)
 
-        return f"Address was deleted"
+        return f"{green}Address was deleted{reset}"
     else:
-        return f"No contact found with name {name}"
+        return f"{red}No contact found with name {name}.{reset}\n"
 
 
 @input_error
@@ -209,15 +214,15 @@ def add_email(args, address_book):
     try:
         name, email = args
     except:
-        raise ValueError("The command is bad. Give me name and email.")
+        raise ValueError(f"{red}The command is bad. Give me name and email.{reset}\n")
 
     record = address_book.find(name)
     if record:
         record.add_email(email)
         address_book.add_record(record)
-        return f"Email added for {name}"
+        return f"{green}Email added for {name}.{reset}"
     else:
-        return f"No contact found with name {name}"
+        return f"{red}No contact found with name {name}.{reset}\n"
 
 
 @input_error
@@ -226,16 +231,18 @@ def change_email(args, address_book):
     try:
         name, old_email, new_email = args
     except:
-        raise ValueError("The command is bad. Give me name, old email and new email.")
+        raise ValueError(
+            f"{red}The command is bad. Give me name, old email and new email.{reset}\n"
+        )
 
     record = address_book.find(name)
 
     if record:
         record.edit_email(old_email, new_email)
         address_book.add_record(record)
-        return "Email changed"
+        return f"{green}Email changed.{reset}"
     else:
-        return f"There isn't a contact with name {name}"
+        return f"{red}There isn't a contact with name {name}.{reset}\n"
 
 
 @input_error
@@ -243,16 +250,16 @@ def show_email(args, address_book):
     try:
         name = args[0]
     except:
-        raise ValueError("The command is bad. Give me name")
+        raise ValueError(f"{red}The command is bad. Give me name.{reset}\n")
 
     record = address_book.find(name)
     if record:
         if record.email:
-            return f"Email of {name}: {record.email}"
+            return f"{green}Email of {name}: {record.email}.{reset}"
         else:
-            return f"Email not set for {name}"
+            return f"{red}Email not set for {name}.{reset}\n"
     else:
-        return f"No contact found with name {name}"
+        return f"{red}No contact found with name {name}.{reset}\n"
 
 
 @input_error
@@ -263,23 +270,23 @@ def delete_email(args, address_book):
         name = args[0]
     except:
 
-        raise ValueError("The command is bad. Give me name.")
+        raise ValueError(f"{red}The command is bad. Give me name.{reset}\n")
     record = address_book.find(name)
     if record:
 
         record.remove_email()
         address_book.add_record(record)
 
-        return f"Email was deleted"
+        return f"{green}Email was deleted.{reset}"
     else:
-        return f"No contact found with name {name}"
+        return f"{red}No contact found with name {name}.{reset}\n"
 
 
 @input_error
 def search(args, address_book):
     found_records = address_book.search(" ".join(args))
     if found_records is None:
-        return "No contacts found matching your search."
+        return f"{red}No contacts found matching your search.{reset}\n"
     else:
         header()
         return "\n".join(found_records)
